1106dda8e1f800bf51cb034b5faab6fafdda0f2d
Gleb Lagutko <gleblagutko@gmail.com>
2019-10-08 19:57:53 +0300

    Initial commit

---
+++ src/test/Lab1.java
@@ -0,0 +1,56 @@
+package test;
+
+public class Lab1 {
+
+    private static long fact(int k) {
+        long answer = 1;
+        for (int i = 1; i < k; i++) {
+            answer *= i;
+        }
+        return answer;
+    }
+
+    private static double progMember(int k, double memb) {
+        double member = memb;
+
+        return member * ((-1) * Math.pow(4, 4)) / ((2 * k + 1) * 2 * k * Math.pow(3, 4));
+    }
+
+    public static void main(String[] args) {
+        check_args(args);
+        String arg1 = args[0];
+        String arg2 = args[1];
+
+
+        double x = parseDouble(arg1, 0);
+        double t = parseDouble(arg2, 0.00000000000001);
+
+
+        double sum = 0;
+
+        double member = (x * x * Math.pow(4, 6)) / (fact(3) * Math.pow(3, 6));
+
+        for (int k = 2; ; k++) {
+            sum += member;
+            member = progMember(k, member);
+            System.out.println(sum);
+            if (Math.abs(member) < t)
+                break;
+        }
+
+
+    }
+
+    private static double parseDouble(String value, double default_value) {
+        try {
+            return Double.parseDouble(value);
+        } catch (Exception e) {
+            return default_value;
+        }
+    }
+
+    private static void check_args(String[] args) {
+        if (args.length != 2)
+            throw new RuntimeException("Wrong argument count ");
+    }
+}
